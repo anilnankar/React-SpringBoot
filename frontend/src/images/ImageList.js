6553893030d028d9d004cd9f3ea730883bf37a5f
Anil Nankar <anil.nankar@aventri.com>
2022-06-30 16:36:03 +0530

    Created React Component and File Handler to upload files

---
+++ frontend/src/images/ImageList.js
@@ -0,0 +1,15 @@
+import './ImageList.css';
+
+function ImageList() {
+  return (
+    <div className="ImageList">
+      <header className="image-upload-header">
+        <h2>
+            Image List
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default ImageList;
